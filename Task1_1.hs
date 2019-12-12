203cadaed827c31a6bcba265a22fe1b710d39c47
ivan <moonlightrider@mail.ru>
2019-12-13 01:03:49 +0300

    done Task1_1

--- Task1_1.hs
+++ Task1_1.hs
@@ -7,26 +7,51 @@ module Task1_1 where
 
 import Todo(todo)
 
+data Op = Add | Subs | Mul
+            deriving(Show,Eq)
+
 data Term = IntConstant{ intValue :: Int }           -- числовая константа
             | Variable{ varName :: String }          -- переменная
-            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
+            | BinaryTerm{ op :: Op, lhv :: Term, rhv :: Term } -- бинарная операция
             deriving(Show,Eq)
+
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
+infixl 6 |+|
+infixl 6 |-|
+infixl 7 |*|
+
 (|+|) :: Term -> Term -> Term
-(|+|) l r = todo
+(|+|) (IntConstant l) (IntConstant r) = IntConstant (l + r)
+(|+|) l r = BinaryTerm Add l r
+
 (|-|) :: Term -> Term -> Term
-(|-|) l r = todo
+(|-|) (IntConstant l) (IntConstant r) = IntConstant (l - r)
+(|-|) l r = BinaryTerm Subs l r
+
 (|*|) :: Term -> Term -> Term
-(|*|) l r = todo
+(|*|) (IntConstant l) (IntConstant r) = IntConstant (l * r)
+(|*|) l r = BinaryTerm Mul l r
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = todo
+replaceVar varName replacement expression = 
+    case expression of 
+         (Variable name)      -> if varName == name 
+                                 then replacement
+                                 else (Variable name)
+         (BinaryTerm o l r)   -> BinaryTerm (o) (replaceVar varName replacement l) (replaceVar varName replacement r) 
+         (IntConstant val)    -> IntConstant val
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate expression = todo
+evaluate (IntConstant val) = IntConstant val
+evaluate BinaryTerm {op = oper, lhv = l, rhv = r} = 
+    case oper of
+         Add  -> evaluate l |+| evaluate r
+         Subs -> evaluate l |-| evaluate r
+         Mul  -> evaluate l |*| evaluate r
+evaluate _ = undefined
