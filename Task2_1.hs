a82361e9a3c888b3fe8469dd48ffbee5ac65934a
VanyaKrylov <moonlightrider@mail.ru>
2020-01-17 11:37:49 +0300

    task 2 done

--- Task2_1.hs
+++ Task2_1.hs
@@ -7,6 +7,7 @@ module Task2_1 where
 -}
 
 import Todo(todo)
+import Data.List hiding (insert, lookup)
 import Prelude hiding (lookup)
 
 -- Ассоциативный массив на основе бинарного дерева поиска
@@ -72,19 +73,48 @@ remove k node@(Node key val ln rn)
 			(EmptyTree, lf@Leaf{k = lkey}) -> if lkey == k 
 											  then Leaf key val
 											  else lf
-			(_		  , _				 ) -> node{r = remove k rn}
+			(_, _)						   -> node{r = remove k rn}
+			
 	| k < key  =
 		case (ln, rn) of
 			(lf@Leaf{k = lkey}, EmptyTree) -> if lkey == k 
 											  then Leaf key val
 											  else lf
-			(_				  , _		 ) -> node{l = remove k ln}
-remove _ _     = undefined
+			(_, _)						   -> node{l = remove k ln}
 
 -- Поиск ближайшего снизу ключа относительно заданного
-nearestLE :: Integer -> TreeMap v -> (Integer, v)
-nearestLE i t = todo
+nearestLE :: Integer -> TreeMap v -> Maybe(Integer, v)
+nearestLE i EmptyTree  =  Nothing
+nearestLE i (Leaf k v) 
+	| i < k  = Nothing
+	| i >= k = Just (k, v)
+nearestLE i (Node k v l r) 
+	| i >= k = Just (k, v)
+	| i < k  = nearestLE i l 
+	
+nearestLE' :: Integer -> TreeMap v -> Maybe(Integer, v)
+nearestLE' i t = res
+	where
+		list      = listFromTree t
+		f z (x,y) = (x-z, y)
+		keyVal    = treeListMin (filter (\x -> (>=0) . fst $ x) (map (f i) list))
+		res = 
+			case keyVal of
+				Nothing     -> keyVal
+				Just(x1,y1) -> Just(x1 + i, y1)--value 	  = lookup key t
 
+treeListMin :: [(Integer, v)] -> Maybe(Integer, v)
+treeListMin tl = func tl Nothing
+	where
+		func [] res   = res
+		func (mK@(k,v):xs) res = 
+			case res of
+				Nothing    -> func xs (Just mK)
+				Just(kk,_) -> if k < kk
+							  then func xs (Just mK)
+							  else func xs res
+
+		
 -- Построение дерева из списка пар
 treeFromList :: [(Integer, v)] -> TreeMap v
 treeFromList lst = foldr insert EmptyTree lst
@@ -96,8 +126,8 @@ listFromTree (Leaf k val)     = [(k,val)]
 listFromTree (Node k val l r) = [(k,val)] ++ (listFromTree l) ++ (listFromTree r)
 
 -- Поиск k-той порядковой статистики дерева
-kMean :: Integer -> TreeMap v -> (Integer, v)
-kMean i t = todo
+kMean :: Int -> TreeMap v -> (Integer, v)
+kMean i t = genericIndex (listFromTree t) i 
 
 lt = Leaf 1 7.22
 lt2 = Leaf 2 "2.22"
