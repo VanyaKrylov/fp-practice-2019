6dd98f64b903c12d1f558ef5fc4b07743c2dc245
VanyaKrylov <studentik7@yandex.ru>
2020-01-18 21:03:49 +0300

    Task 3_1 done

--- Task3_1.hs
+++ Task3_1.hs
@@ -8,4 +8,83 @@ module Task3_1 where
 
 data WeirdPeanoNumber = Zero | Succ WeirdPeanoNumber | Pred WeirdPeanoNumber
 
+x11 = Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ Zero))))))))))
+x10 = Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ (Succ Zero)))))))))
+x2 = Succ (Succ Zero)
+y2 = Succ (Succ Zero)
+x2' = Pred (Pred Zero)
+
 -- Реализуйте все классы типов, которым должны отвечать целые числа
+instance Eq WeirdPeanoNumber where
+    Zero   == Zero = True
+    Zero   == _    = False
+    _      == Zero = False
+    Succ a == Succ b = (==) a b
+    Pred a == Pred b = (==) a b
+    _      == _      = False
+
+instance Ord WeirdPeanoNumber where
+    Zero   <= Zero   = True
+    Pred _ <= Zero   = True
+    Zero   <= Pred _ = False
+    Pred a <= Pred b = (<=) a b
+    Pred _ <= _      = True
+    Succ _ <= Zero   = False
+    Zero   <= Succ _ = True
+    Succ a <= Succ b = (<=) a b
+    Succ _ <= _      = False
+    
+instance Show WeirdPeanoNumber where
+    show Zero   = "Zero"
+    show (Succ a) = "(Succ " ++ (show a) ++ ")"
+    show (Pred a) = "(Pred " ++ (show a) ++ ")"
+    
+instance Enum WeirdPeanoNumber where
+    toEnum id 
+        | id == 0 = Zero
+        | id > 0  = consSucc id 0 Zero
+        | id < 0  = consPred id 0 Zero
+            where
+                consSucc x c v
+                    | c == x = v
+                    | c < x  = consSucc x (c+1) (Succ v)
+                    
+                consPred x c v
+                    | c == x = v
+                    | c > x  = consPred x (c-1) (Pred v)
+                    
+    fromEnum val = 
+        case val of
+            Zero   -> 0
+            Succ a -> 1 + fromEnum a 
+            Pred a -> (-1) + fromEnum a
+
+instance Num WeirdPeanoNumber where
+    (+) Zero a = a
+    (+) a Zero = a
+    (+) a b    = toEnum $ fromEnum a + fromEnum b
+    
+    negate a = toEnum $ (-1) * fromEnum a
+    
+    (*) a b = toEnum $ fromEnum a * fromEnum b
+    
+    abs a 
+        | a == Zero = a
+        | otherwise = negate a
+    
+    signum a 
+        | a == Zero = Zero
+        | a >= Zero = Succ Zero
+        | otherwise = Pred Zero
+    
+    fromInteger int = toEnum . fromIntegral $ int 
+    
+
+
+
+
+
+
+
+
+        
