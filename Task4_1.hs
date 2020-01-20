4850b1df90d0b5ece4b70bd3096ec7b7506aacad
VanyaKrylov <moonlightrider@mail.ru>
2020-01-20 15:48:00 +0300

    task 4

--- Task4_1.hs
+++ Task4_1.hs
@@ -5,11 +5,36 @@ module Task4_1 where
   Реализация монады над функцией.
   Реализуйте все требуемые классы типов и объясните, почему они реализованы именно так.
 -}
-
+import Data.Char
 -- Монада над функцией. В качестве входного значения `fun` может быть что угодно
 -- Собственно, почему бы не `String`?
 data FunMonad a = FunMonad { fun :: String -> a }
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FunMonad`
+
+-- fmap :: (a -> b) -> f a -> f b
+-- В качестве аргумента "а" передаемой в fmap ф-ции служит результат выполнения ф-ции,
+-- обёрнутой в FunMonad, т.о. для реализации fmap достаточно выполнить композицию ф-ций
+-- (a -> b) . (String -> a) = b
+-- И на последнем шаге оборачиваем полученное значение обратно в контейнер FunMonad
+-- *Task4_1> let f = map(map toUpper)
+-- *Task4_1> let fm1 = FunMonad words
+-- *Task4_1> f((fun fm1) "Hello World")
+-- ["HELLO","WORLD"]
 instance Functor FunMonad where
     fmap f (FunMonad fu) = FunMonad (f . fu)
+
+    
+instance Applicative FunMonad where
+    pure x                           = FunMonad (\s -> x)
+    (<*>) (FunMonad mf) (FunMonad m) = FunMonad (\s -> (mf s) $ (m s))
+    
+instance Monad FunMonad where
+    (>>=) (FunMonad m) k = FunMonad (\s -> case m s of ss -> (fun $ k $ ss) s)
+{-
+(>>=) :: FunMonad (String -> a) 
+         -> (a -> FunMonad (String -> b))
+         -> FunMonad (String -> b)
+         -}
+--func :: (a -> b) -> (b -> b) -> a -> b
+--func f ff a = case f a of x -> ff x
--- Task4_2.hs
+++ Task4_2.hs
@@ -6,10 +6,23 @@ module Task4_2 where
   Реализуйте все требуемые классы типов и объясните, почему они реализованы именно так.
 -}
 
-data FourOf a = FourOf a a a a deriving(Show,Eq)
+data FourOf a = FourOf {fi::a, sn::a, th::a, fo::a} deriving(Show,Eq)
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FourOf`
 -- таким образом, что
 -- do { x <- FourOf 1 2 3 4; y <- FourOf 4 6 7 8; return $ x + y } === FourOf 5 8 10 12
 instance Functor FourOf where
     fmap f (FourOf a b c d) = FourOf (f a) (f b) (f c) (f d)
+
+instance Applicative FourOf where
+    pure x = FourOf x x x x
+    (<*>) (FourOf fa fb fc fd) (FourOf a b c d) = FourOf (fa a) (fb b) (fc c) (fd d)  
+    
+{- FourOf 1 2 3 4 >>= \x ->
+   FourOf 4 6 7 8 >>= \y ->
+   return \z ->
+   x+y
+-}
+instance Monad FourOf where
+    (>>=) m k = FourOf{fi=fi $ k $ fi $ m, sn=sn $ k $ sn $ m,
+                       th=th $ k $ th $ m, fo=fo $ k $ fo $ m}
